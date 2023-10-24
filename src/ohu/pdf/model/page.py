91ce8fb2f1e85d582f0fd5df272a42677256d16c
adama <adhambek.otabekov.post@gmail.com>
2023-10-24 03:24:16 +0200

    minor

--- src/ohu/pdf/model/page.py
+++ src/ohu/pdf/model/page.py
@@ -8,67 +8,81 @@ class Page(QtCore.QObject):
     annotationAdded=QtCore.pyqtSignal(object)
     annotationRemoved=QtCore.pyqtSignal(object)
 
-    def __init__(self, popplerPage, pageNumber, document):
+    def __init__(
+            self, 
+            popplerPage, 
+            pageNumber, 
+            document
+            ):
 
         super(Page, self).__init__()
-
         self.m_annotations=[]
         self.m_document=document
         self.m_data = popplerPage
         self.m_pageNumber=pageNumber
-
         self.m_normalizedTransform=QtGui.QTransform()
         self.m_normalizedTransform.reset()
         self.m_normalizedTransform.scale(
                 self.size().width(), 
                 self.size().height())
-
         self.m_native_annotations=self.getNativeAnnotations()
 
-    def document(self): return self.m_document
+    def document(self): 
+        return self.m_document
 
-    def pageNumber(self): return self.m_pageNumber
+    def pageNumber(self): 
+        return self.m_pageNumber
 
-    def pageItem(self): return self.m_pageItem
+    def pageItem(self): 
+        return self.m_pageItem
 
-    def setPageItem(self, pageItem): self.m_pageItem=pageItem
+    def setPageItem(self, pageItem): 
+        self.m_pageItem=pageItem
 
-    def size(self): return self.m_data.pageSizeF()
+    def size(self): 
+        return self.m_data.pageSizeF()
 
-    def render(self, hResol=72, vResol=72, rotate=0, boundingRect=None):
+    def render(
+            self, 
+            hResol=72, 
+            vResol=72, 
+            rotate=0, 
+            boundingRect=None
+            ):
 
         x, y, w, h = (-1,)*4
-
-        if boundingRect is not None:
+        if boundingRect:
             x = int(boundingRect.x())
             y = int(boundingRect.y())
             w = int(boundingRect.width())
             h = int(boundingRect.height())
 
-        return self.m_data.renderToImage(hResol, vResol, x, y, w, h, rotate)
+        return self.m_data.renderToImage(
+                hResol, vResol, x, y, w, h, rotate)
 
     def find(self, rect, unified=False): 
 
-        if unified: rect=self.m_normalizedTransform.mapRect(rect)
+        if unified: 
+            rect=self.m_normalizedTransform.mapRect(rect)
         return self.m_data.text(rect)
 
     def search(self, string): 
 
-        return self.m_data.search(string, Poppler.Page.CaseInsensitive)
+        return self.m_data.search(
+                string, Poppler.Page.CaseInsensitive)
 
     def annotate(self, aData, kind, **kwargs):
 
-        color=aData['color']
-        boundary=aData['boundaries']
-
+        c=aData['color']
+        b=aData['boundaries']
         if kind=='highlightAnnotation':
-            annotation=self.addHighlightAnnotation(boundary, color)
+            annotation=self.addHighlightAnnotation(
+                    b, c)
         elif kind=='textAnnotation':
-            annotation=self.addTextAnnotation(boundary, color)
-
+            annotation=self.addTextAnnotation(
+                    b, c)
         aData['pAnn']=annotation
         self.m_annotations+=[aData]
-
         return aData
 
     def addHighlightAnnotation(self, boundary, color):
@@ -107,13 +121,11 @@ class Page(QtCore.QObject):
         popup=Poppler.Annotation.Popup()
         popup.setFlags(Poppler.Annotation.Hidden or
                 Poppler.Annotation.ToggleHidingOnMouse)
-
-        annotation=Poppler.TextAnnotation(Poppler.TextAnnotation.Linked)
-
+        annotation=Poppler.TextAnnotation(
+                Poppler.TextAnnotation.Linked)
         annotation.setBoundary(boundary)
         annotation.setStyle(style)
         annotation.setPopup(popup)
-
         self.m_data.addAnnotation(annotation)
         ann=Annotation(annotation)
         ann.setPage(self)
@@ -121,22 +133,22 @@ class Page(QtCore.QObject):
 
     def removeAnnotation(self, aData):
 
-        aid=aData.get('id')
+        aid=aData.get('id', None)
+        if not aid: return
+        for d in self.m_annotations:
+            if d['id']==aid:
+                adx=self.m_annotations.index(d)
+                self.m_annotations.pop(adx)
+                self.m_data.removeAnnotation(
+                        d['pAnn'].data())
+                self.annotationRemoved.emit(self)
 
-        if aid:
-
-            for data in self.m_annotations:
-                if data['id']==aid:
-                    self.m_annotations.pop(self.m_annotations.index(data))
-                    self.m_data.removeAnnotation(data['pAnn'].data())
-                    self.annotationRemoved.emit(self)
-
-    def nativeAnnotations(self): return self.m_native_annotations
+    def nativeAnnotations(self): 
+        return self.m_native_annotations
 
     def getNativeAnnotations(self):
 
         annotations=[]
-
         for data in self.m_data.annotations():
             kind = data.subType()
             cond = kind in [
@@ -148,10 +160,10 @@ class Page(QtCore.QObject):
                 annotation=Annotation(data)
                 annotation.setPage(self)
                 annotations+=[annotation]
-
         return annotations
 
-    def annotations(self): return self.m_annotations
+    def annotations(self): 
+        return self.m_annotations
 
     def links(self):
 
@@ -172,13 +184,10 @@ class Page(QtCore.QObject):
                     left = (left > 1.)*1.+(left < 0.)*0.
                 if not 0 <= top <= 1:
                     top = (top > 1.)*1.+(top < 0.)*0.
-
                 if linkGoto.isExternal():
                     data={'boundary': boundary, 'file': linkGoto.fileName(), 'page': page}
                 else:
                     data={'boundary': boundary, 'page': page, 'left': left, 'top': top}
-
-
             elif link.linkType() == Poppler.Link.Browse:
                 linkBrowse = link
                 url = linkBrowse.url()
@@ -186,15 +195,13 @@ class Page(QtCore.QObject):
             elif link.linkType() == Poppler.Link.Execute:
                 url = link.fileName()
                 data={'boundary': boundary, 'path': url}
-
             if data:
-
                 data['sourcePage'] = self.pageNumber()
                 links.append(data)
-
         return links
 
-    def data(self): return self.m_data
+    def data(self): 
+        return self.m_data
 
     def getRows(self, start, end):
 
@@ -207,50 +214,52 @@ class Page(QtCore.QObject):
                 rect.setTopLeft(end.topLeft())
                 rect.setBottomRight(start.bottomRight())
             area=[rect]
-
         else:
-
             if start.y()<end.y():
                 up, down=start, end
             else:
                 up, down=end, start
-
             rects={}
             for b in self.data().textList():
                 box=b.boundingBox()
                 if box.y()>=up.y() and box.y()<=down.y():
                     if box.y()==up.y():
-                        if box.x()<up.x(): continue
+                        if box.x()<up.x(): 
+                            continue
                     if box.y()==down.y():
-                        if box.x()+box.width()>down.x()+down.width(): continue
-
-                    if not box.y() in rects: rects[box.y()]=box
-
+                        if box.x()+box.width()>down.x()+down.width(): 
+                            continue
+                    if not box.y() in rects: 
+                        rects[box.y()]=box
                     r=rects[box.y()]
-
                     if box.x()<r.x(): 
                         r.setTopLeft(box.topLeft())
-
                     if box.x()+box.width()>r.x()+r.width():
                         r.setBottomRight(box.bottomRight())
-
             area=list(rects.values())
-
         text=[]
-
-        for a in area: text+=[self.find(a)]
-
+        for a in area: 
+            text+=[self.find(a)]
         data=[]
         for t in self.data().textList():
             for a in area: 
                 if a.contains(t.boundingBox()): data+=[t]
-
-        return {'box':area, 'text': ' '.join(text), 'data':data}
+        return {
+                'box':area, 
+                'lines': text,
+                'text': ' '.join(text), 
+                'data':data
+                }
 
     def getRow(self, point):
 
         for tData in self.data().textList():
             if tData.boundingBox().contains(point):
-                return {'data': [tData], 'box':[tData.boundingBox()], 'text':tData.text()}
+                return {
+                        'data': [tData], 
+                        'box':[tData.boundingBox()], 
+                        'text':tData.text()
+                        }
 
-    def textList(self): return self.data().textList()
+    def textList(self): 
+        return self.data().textList()
--- src/ohu/pdf/view/cursor.py
+++ src/ohu/pdf/view/cursor.py
@@ -4,42 +4,52 @@ class Cursor(QObject):
 
     def __init__(self, view): 
 
-        super().__init__(view)
-
         self.start=None
-
-        view.itemMouseMoveOccured.connect(self.on_mouseMove)
-        view.itemMousePressOccured.connect(self.on_mousePress)
-        view.itemMouseReleaseOccured.connect(self.on_mouseRelease)
-        view.itemMouseDoubleClickOccured.connect(self.on_doubleClick)
+        super().__init__(view)
+        view.itemMouseMoveOccured.connect(
+                self.on_mouseMove)
+        view.itemMousePressOccured.connect(
+                self.on_mousePress)
+        view.itemMouseReleaseOccured.connect(
+                self.on_mouseRelease)
+        view.itemMouseDoubleClickOccured.connect(
+                self.on_doubleClick)
 
     def move(self, event, item):
 
-        point=item.mapToPage(event.pos(), unify=False)
-        current=item.page().getRow(point)
-
-        if self.start and current:
-
-            selection=item.page().getRows(
-                    self.start['box'][0], current['box'][0])
-            item.select([selection])
+        point=item.mapToPage(
+                event.pos(), unify=False)
+        c=item.page().getRow(point)
+        if self.start and c:
+            s=item.page().getRows(
+                    self.start['box'][0], 
+                    c['box'][0]
+                    )
+            item.select([s])
 
     def on_mousePress(self, view, item, event):
 
+        page=item.page()
         item.prev_cursor=item.cursor()
         item.setCursor(Qt.IBeamCursor)
+        point=item.mapToPage(
+                event.pos(), unify=False)
+        self.start=page.getRow(
+                point)
 
-        point=item.mapToPage(event.pos(), unify=False)
-        self.start=item.page().getRow(point)
+    def on_mouseMove(self, view, item, event): 
+        self.move(event, item)
 
-    def on_mouseMove(self, view, item, event): self.move(event, item)
-
-    def on_mouseRelease(self, view, item, event): item.setCursor(item.prev_cursor)
+    def on_mouseRelease(self, view, item, event): 
+        item.setCursor(item.prev_cursor)
 
     def on_doubleClick(self, view, item, event):
 
         selection=[]
-        point=item.mapToPage(event.pos(), unify=False)
-        data=item.page().getRow(point)
-        if data: selection+=[data] 
+        page=item.page()
+        point=item.mapToPage(
+                event.pos(), unify=False)
+        data=page.getRow(point)
+        if data: 
+            selection+=[data] 
         item.select(selection)
