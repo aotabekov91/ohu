dfe6979695b582a4646ed8bc9da5e3ec8e7b3dcd
adam <adam@myworld.net>
2023-10-25 04:58:45 +0200

    cleanup

--- src/ohu/pdf/view/view.py
+++ src/ohu/pdf/view/view.py
@@ -15,14 +15,23 @@ class View(BaseView):
             bool, object)
 
     def __init__(
-            self, app, layout=Layout):
+            self, 
+            app, 
+            layout=Layout,
+            **kwargs,
+            ):
 
-        super().__init__(app, layout)
         self.m_prev = 1 
         self.m_curr = 1 
         self.m_items=[]
+        self.m_pages=[]
+        self.delta = 0.05
         self.s_cache = {}
         self.m_paintlinks=False
+        super().__init__(
+                app=app, 
+                layout=layout, 
+                **kwargs)
         self.cursor=Cursor(self)
 
     def initialize(self):
@@ -39,7 +48,7 @@ class View(BaseView):
         l, t=self.getPosition()
         self.updateSceneAndView(l, t)
 
-    def getPosition(self, left=0., top=0.):
+    def getPosition(self):
 
         p=self.m_items[self.m_curr-1]
         r=p.boundingRect().translated(p.pos())
@@ -48,16 +57,6 @@ class View(BaseView):
         left=(topLeft.x() -r.x())/r.width()
         top=(topLeft.y() -r.y())/r.height()
         return left, top
-
-    def next(self): 
-
-        self.goto(self.m_layout.nextPage(
-            self.m_curr, len(self.m_pages)))
-
-    def prev(self):
-
-        self.goto(self.m_layout.previousPage(
-            self.m_curr, len(self.m_pages)))
 
     def setModel(self, model):
 
@@ -79,7 +78,9 @@ class View(BaseView):
             self, 
             changeLeft=0., 
             changeTop=0., 
-            visiblePage=0):
+            visiblePage=0,
+            goto=False,
+            ):
 
         r = self.scene().sceneRect()
         left = r.left()
@@ -102,28 +103,26 @@ class View(BaseView):
                 else:
                     p.setVisible(False)
                     p.cancelRender()
-
             if idx == visiblePage-1:
                 horizontalValue = int(
                     brect.left()+changeLeft*brect.width())
                 verticalValue = int(
                     brect.top()+changeTop*brect.height())
-
         #raise highlightIsOnPage
-
-        self.setSceneRect(left, top, width, height)
-        self.horizontalScrollBar().setValue(horizontalValue)
-        self.verticalScrollBar().setValue(verticalValue)
+        self.setSceneRect(
+                left, top, width, height)
+        self.horizontalScrollBar().setValue(
+                horizontalValue)
+        self.verticalScrollBar().setValue(
+                verticalValue)
         self.viewport().update()
 
     def reportPosition(self):
 
-        left, top = self.getPosition()
+        l, t = self.getPosition()
+        item=self.pageItem()
         self.positionChanged.emit(
-                self, 
-                self.pageItem(), 
-                left, 
-                top)
+                self, item, l, t) 
 
     def prepareScene(self, w, h):
 
@@ -164,10 +163,10 @@ class View(BaseView):
 
         self.m_items = []
         for i, page in enumerate(self.m_pages):
-            pageItem = Item(page, self)
-            page.setPageItem(pageItem)
-            self.m_items += [pageItem]
-            self.scene().addItem(pageItem)
+            item = Item(page, self)
+            page.setPageItem(item)
+            self.m_items += [item]
+            self.scene().addItem(item)
 
     def adjustScrollBarPolicy(self):
 
@@ -186,98 +185,22 @@ class View(BaseView):
             if self.s_settings.get('continuousView', True):
                 policy = QtCore.Qt.ScrollBarAsNeeded
 
-    def down(self):
-
-        visibleHeight=self.m_layout.visibleHeight(self.size().height())*.05
-        dx=self.verticalScrollBar().value() + visibleHeight
-        h=int(self.scene().sceneRect().height())
-        self.verticalScrollBar().setValue(h)
-        if dx<=self.scene().sceneRect().height():
-            self.verticalScrollBar().setValue(int(dx-0.5))
-        self.setFromVisible()
-
-    def up(self):
-
-        visibleHeight=self.m_layout.visibleHeight(
-                self.size().height())*.05
-        dx=self.verticalScrollBar().value() - visibleHeight
-        self.verticalScrollBar().setValue(0)
-        if dx>=0:
-            self.verticalScrollBar().setValue(int(dx+0.5))
-        self.setFromVisible()
-        
-    def left(self):
-
-        self.horizontalScrollBar().setValue(
-                int(self.horizontalScrollBar().value()*1.1))
-        self.setFromVisible()
-
-    def right(self):
-
-        self.horizontalScrollBar().setValue(
-                int(self.horizontalScrollBar().value()*0.9))
-        self.setFromVisible()
-
-    def pageUp(self):
-
-        visibleHeight=self.m_layout.visibleHeight(self.size().height())
-        dx=self.verticalScrollBar().value() - visibleHeight
-        if dx>=0:
-            self.verticalScrollBar().setValue(int(dx+5))
-        else:
-            self.verticalScrollBar().setValue(0)
-        self.setFromVisible()
-
-    def pageDown(self):
-
-        visibleHeight=self.m_layout.visibleHeight(self.size().height())
-        dx=self.verticalScrollBar().value() + visibleHeight
-        if dx<=self.scene().sceneRect().height():
-            self.verticalScrollBar().setValue(int(dx-5))
-        else:
-            self.verticalScrollBar().setValue(int(self.scene().sceneRect().height()))
-        self.setFromVisible()
-
-    def setFromVisible(self):
+    def setVisiblePage(self):
 
         r=self.viewport().rect()
-        v=QtCore.QRect(
-                int(r.width()/2)-5, int(r.height()/2-5), 10, 10)
+        x, w = int(r.width()/2-5), 10
+        y, h =int(r.height()/2-5), 10
+        v=QtCore.QRect(x, y, w, h)
         items=self.items(v)
         if items:
-            self.setCurrentPage(
-                    items[0].page().pageNumber())
-        self.reportPosition()
+            page=items[0].page()
+            pnum=page.pageNumber()
+            self.setCurrentPage(pnum)
 
     def scaleMode(self):
 
         return self.s_settings.get(
                 'scaleMode', 'FitToPageHeight')
-
-    def zoomIn(self, digit=1):
-
-        for i in range(digit):
-            self._zoom(kind='in')
-
-    def zoomOut(self, digit=1): 
-
-        for i in range(digit):
-            self._zoom(kind='out')
-
-    def _zoom(self, kind='out'):
-
-        zoomFactor = self.s_settings.get('zoomFactor', .1)
-        if self.scaleMode() != 'ScaleFactor': 
-            self.setScaleMode('ScaleFactor')
-        if kind=='out':
-            zoomFactor=1.-zoomFactor
-        elif kind=='in':
-            zoomFactor=1.+zoomFactor
-        l, p = self.getPosition()
-        for item in self.m_items: 
-            item.setScaleFactor(
-                    zoomFactor*item.scale())
-        self.updateSceneAndView(left=l, top=p)
 
     def setScaleFactor(self, scaleFactor):
 
@@ -290,40 +213,38 @@ class View(BaseView):
                 left, top = self.getPosition()
                 self.updateSceneAndView(left=left, top=top)
 
-    def fitToPageWidth(self):
-        self.setScaleMode('FitToPageWidth')
-
-    def fitToPageHeight(self):
-        self.setScaleMode('FitToPageHeight')
-
     def setScaleMode(self, scaleMode):
 
         self.s_settings['scaleMode'] = scaleMode
-        left, top = self.getPosition()
-        self.getPosition(left, top)
+        l, t = self.getPosition()
         self.adjustScrollBarPolicy()
-        self.updateSceneAndView()
-        self.scaleModeChanged.emit(scaleMode, self)
+        self.updateSceneAndView(l, t)
+        self.scaleModeChanged.emit(
+                scaleMode, self)
 
     def updateSceneAndView(
             self, 
             left=None, 
-            top=None):
+            top=None
+            ):
 
-        if not left or not top:
-            left, top = self.getPosition()
-        visibleWidth=self.m_layout.visibleWidth(
-                self.size().width())
-        visibleHeight=self.m_layout.visibleHeight(
-                self.size().height())
-        self.prepareScene(
-                visibleWidth, visibleHeight)
-        self.prepareView(
-                left, top)
+        l, t = self.getPosition()
+        top = top or t
+        left = left or l 
+        s=self.size()
+        l=self.m_layout
+        vw=l.visibleWidth(s.width())
+        vh=l.visibleHeight(s.height())
+        self.prepareScene(vw, vh)
+        self.prepareView(left, top)
 
-    def save(self, path=False, refresh=True):
+    def save(
+            self, 
+            path=None, 
+            refresh=True
+            ):
 
-        if path is False: 
+        if not path: 
             path=self.m_model.filePath()
         tFile=QtCore.QTemporaryFile()
         if tFile.open(): 
@@ -346,11 +267,13 @@ class View(BaseView):
         self.m_prev=self.m_curr
         self.m_curr=pnum
         if self.m_prev!=self.m_curr:
-            item=self.pageItem()
+            i=self.pageItem()
             self.itemChanged.emit(
-                    self, item)
+                    self, i) 
+        self.reportPosition()
 
-    def setPaintLinks(self, condition=True):
+    def setPaintLinks(
+            self, condition=True):
 
         self.m_paintlinks=condition
         for item in self.m_items:
@@ -380,12 +303,6 @@ class View(BaseView):
             return self.m_model.hash()
         return super().name()
 
-    def gotoEnd(self): 
-        self.goto(len(self.m_pages))
-
-    def gotoBegin(self): 
-        self.goto(1)
-
     def currentPage(self): 
         return self.m_curr
 
@@ -395,19 +312,126 @@ class View(BaseView):
     def paintLinks(self): 
         return self.m_paintlinks
 
+    def fitToPageWidth(self):
+        self.setScaleMode('FitToPageWidth')
+
+    def fitToPageHeight(self):
+        self.setScaleMode('FitToPageHeight')
+
+    def gotoFirst(self): 
+        self.goto(1)
+
+    def pageUp(self, digit=1):
+        self.movePage('up', digit)
+
+    def pageDown(self, digit=1):
+        self.movePage('down', digit)
+
+    def zoomIn(self, digit=1):
+        self.setZoom('in', digit)
+
+    def zoomOut(self, digit=1): 
+        self.setZoom('out', digit)
+        
+    def down(self, digit=1):
+        self.move('down', digit)
+
+    def up(self, digit=1):
+        self.move('up', digit)
+
+    def left(self, digit=1):
+        self.move('left', digit)
+
+    def right(self, digit=1):
+        self.move('right', digit)
+
     def goto(
             self, 
-            page=1, 
+            page=None, 
             left=0., 
             top=0.
             ):
 
+        l, t = self.getPosition()
+        page = page or len(self.m_pages)
         if page >= 0 and page <= len(self.m_pages):
-            l, t = self.getPosition()
             cpage=self.m_layout.currentPage(page)
             c = self.m_curr != cpage 
             c = any([c, abs(l-left) > 0.001])
             c = any([c, abs(t-top) > 0.001])
             if c:
-                self.prepareView(left, top, page)
-                self.setFromVisible()
+                self.prepareView(
+                        left, top, page, goto=True)
+                self.setCurrentPage(page)
+                self.setVisiblePage()
+                
+
+    def movePage(self, kind, digit=1):
+
+        h=self.size().height()
+        bar=self.verticalScrollBar()
+        vh=self.m_layout.visibleHeight(h)
+        sh=self.scene().sceneRect().height()
+        if kind=='up':
+            dx=bar.value() - vh*digit
+            dx=max(0, dx) 
+        elif kind=='down':
+            dx=bar.value() + vh*digit
+            dx=min(sh, dx) 
+        bar.setValue(int(dx))
+        self.setVisiblePage()
+
+
+    def setZoom(self, kind='out', digit=1):
+
+        if self.scaleMode() != 'ScaleFactor': 
+            self.setScaleMode('ScaleFactor')
+        zf = self.s_settings.get(
+                'zoomFactor', .1)
+        if kind=='out':
+            zf=(1.-zf)**digit
+        elif kind=='in':
+            zf=(1.+zf)**digit
+        l, t = self.getPosition()
+        for item in self.m_items: 
+            n_zf=zf*item.scale()
+            item.setScaleFactor(n_zf)
+        self.updateSceneAndView(l, t)
+
+    def move(self, kind, digit=1):
+
+        s=self.size()
+        l=self.m_layout
+        sr=self.scene().sceneRect()
+        vbar=self.verticalScrollBar()
+        hbar=self.horizontalScrollBar()
+        vw=l.visibleHeight(s.width())
+        vh=l.visibleHeight(s.height())
+        inc_vh=vh*self.delta
+        inc_vw=vw*self.delta
+
+        if kind=='down':
+            dx=vbar.value() + inc_vh*digit
+            dx=min(sr.height(), dx)
+            vbar.setValue(int(dx))
+        elif kind=='up':
+            dx=vbar.value() - inc_vh*digit
+            dx=max(0, dx)
+            vbar.setValue(int(dx))
+        elif kind=='right':
+            dx=hbar.value() + inc_vw*digit
+            hbar.setValue(int(dx))
+        elif kind=='left':
+            dx=hbar.value() - inc_vw*digit
+            hbar.setValue(int(dx))
+        self.setVisiblePage()
+        
+    def next(self): 
+
+        self.goto(self.m_layout.nextPage(
+            self.m_curr, len(self.m_pages)))
+
+    def prev(self):
+
+        self.goto(self.m_layout.previousPage(
+            self.m_curr, len(self.m_pages)))
