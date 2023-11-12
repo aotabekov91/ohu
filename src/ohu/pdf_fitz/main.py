be6916f441c9ea4d450aa3fc2b457981fd0227cc
adam <adam@myworld.net>
2023-11-12 22:34:08 +0100

    added unique/view_positiin props

--- src/ohu/pdf_fitz/main.py
+++ src/ohu/pdf_fitz/main.py
@@ -1,9 +1,11 @@
-from plug.qt.plugs.render import DisplayRender
+from plug.qt.plugs.render import Render
 
 from .view import FitzView
 from .model import FitzModel
 
-class PdfFitz(DisplayRender):
+class PdfFitz(Render):
 
+    unique=False
     view_class=FitzView
     model_class=FitzModel
+    view_position='display'
