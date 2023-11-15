e95a7c99ada985eafef65fdb7f0c41e0afe56c00
adam <adam@myworld.net>
2023-11-15 17:20:57 +0100

    updated file_browser model

--- src/ohu/file_browser/model/main.py
+++ src/ohu/file_browser/model/main.py
@@ -1,13 +1,24 @@
-import os
+from os.path import abspath
 from PyQt5.QtWidgets import QFileSystemModel
 
 class FileBrowserModel(QFileSystemModel):
 
     kind='files'
+    root_path='/'
 
-    def getPath(self, idx=None):
+    def __init__(self, source='/'):
 
-        idx=idx or self.ui.currentIndex()
-        p=self.filePath(idx)
-        if os.path.exists(p): 
-            return p
+        super().__init__()
+        self.m_data=source
+        self.m_source=source
+        self.setRootPath(self.root_path)
+
+    def getPathIndex(self, path=None):
+
+        path = path or abspath('.')
+        return self.index(path)
+
+    def element(self, idx):
+
+        idx=idx or self.currentIndex()
+        return self.filePath(idx)
