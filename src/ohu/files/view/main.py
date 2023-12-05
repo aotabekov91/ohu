8a23d37801b7c06c4084dc842e798d07e67c4cda
adam <adam@myworld.net>
2023-12-05 22:36:21 +0100

    files minor update with copying

--- src/ohu/files/view/main.py
+++ src/ohu/files/view/main.py
@@ -8,6 +8,18 @@ class FTab(view.Tabber):
     tab_class=FilesView
     prefix_keys={'command': 'f'}
     position={'FTab': 'dock_left'}
+
+    def tabAddNew(self, copy=False):
+
+        ptab=self.current_tab
+        if ptab:
+            ntab=ptab.copy()
+        else:
+            ntab=self.tabGet()
+        self.tabAdd(ntab)
+        self.tabSet(ntab)
+        self.setFocus()
+        return ntab
     
     @tag('t', modes=['command'])
     def toggle(self):
--- src/ohu/files/view/mixin/copy.py
+++ src/ohu/files/view/mixin/copy.py
@@ -5,14 +5,15 @@ class Copy:
     def copy(self):
 
         c=self.__class__(
-               name=self.name,
-               index=self.index,
-               config=self.config,
-               source=self.source)
+                app=self.app, 
+                name=self.name, 
+                index=self.m_id, 
+                config=self.m_config)
         c.setModel(self.m_model)
         self.app.handler.connectView(c)
         idx=self.currentIndex()
         path=self.m_model.filePath(idx)
         idx=self.m_model.getPathIndex(path)
-        c.setRootIndex(idx)
+        c.setRootIndex(idx.parent())
+        c.setCurrentIndex(idx)
         return c
